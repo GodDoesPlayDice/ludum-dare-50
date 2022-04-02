0c1f994135cb0643dabb8b239d22d72c72f31694
GodDoesPlayDice <alik_okroyan@mail.ru>
2022-04-02 13:38:22 +0400

    project dummy

---
+++ Assets/__Commons/Scenes/Scripts/SceneEnum.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum SceneEnum
+{
+
+    CONTROLLER = 0,
+    SHARED_DATA = 1,
+    MUSIC = 2,
+    TITLE = 3,
+    NULL = 9999
+}
