b89b8fbdda68c57d09f2e2c39d78352b29721f8d
GodDoesPlayDice <alik_okroyan@mail.ru>
2022-04-04 21:53:00 +0400

    towers are giving products now

---
+++ Assets/__Game/Scripts/Environment/ProjectileController.cs
@@ -0,0 +1,29 @@
+using System;
+using __Game.Scripts.Actors;
+using UnityEngine;
+
+namespace __Game.Scripts.Environment
+{
+    public class ProjectileController : MonoBehaviour
+    {
+        public GoodType productSO;
+
+        private float liveTime = 0;
+        private void OnTriggerEnter(Collider other)
+        {
+            other.gameObject.TryGetComponent<MobController>(out var mobController);
+            if (mobController != null)
+            {
+                mobController.GiveProduct(productSO, 1);
+            }
+        }
+        
+
+        private void FixedUpdate()
+        {
+            if (transform.position.y <= -2) Destroy(gameObject);
+            liveTime += Time.fixedDeltaTime;
+            if (liveTime > 2 ) Destroy(gameObject);
+        }
+    }
+}
