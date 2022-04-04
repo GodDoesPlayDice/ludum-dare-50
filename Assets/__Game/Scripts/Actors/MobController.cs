ca7b3cd0ead65878d59e3851ccc8ec90d8082f8f
GastarTestProjects <53343040+GastarTestProjects@users.noreply.github.com>
2022-04-04 12:56:02 +0300

    Add products required to customer

--- Assets/__Game/Scripts/Actors/MobController.cs
+++ Assets/__Game/Scripts/Actors/MobController.cs
@@ -1,9 +1,33 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace __Game.Scripts.Actors
 {
     public class MobController : MonoBehaviour
     {
-        
+        #region Fields
+
+        public Dictionary<GoodType, int> productsRequire;
+
+        #endregion
+
+        public bool GiveProduct(GoodType type, int count)
+        {
+            if (productsRequire.ContainsKey(type) && productsRequire[type] > 0)
+            {
+                var requireLeft = productsRequire[type] - count;
+                if (requireLeft > 0)
+                {
+                    productsRequire[type] = requireLeft;
+                }
+                else
+                {
+                    productsRequire.Remove(type);
+                }
+                return true;
+            }
+
+            return false;
+        }
     }
 }
